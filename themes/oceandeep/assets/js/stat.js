a126b5a9b19e949938270ba3ae09a856436b82e6
ruanyf <yifeng.ruan@gmail.com>
2017-08-07 13:36:48 +0800

    feat: add stats

---
+++ themes/oceandeep/assets/js/stat.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d8e2c512b1d4eda64695e7cc41a2cb20";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
